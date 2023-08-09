35d9707a77b63447683a44de0ae07c5540569494
yusufffbal <213301074@ogr.selcuk.edu.tr>
2023-08-09 16:26:14 +0300

    mm

--- lib/services/BiletService.dart
+++ lib/services/BiletService.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: file_names
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -10,31 +9,29 @@ class BiletService with ChangeNotifier {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<void> saveReservation(BiletModel bilet) {
-    return firestore
-    .collection('biletler')
-    .doc(bilet.id)
-    .set(bilet.toMap());
+    return firestore.collection('biletler').doc(bilet.id).set(bilet.toMap());
     // Firestore Database'e ekleme islemini gerceklestirmek icin gereken fonksiyondur.
   }
 
-  Stream<List<BiletModel>> getBiletler()
-  {
-    return firestore
-    .collection('biletler').snapshots().map((snapshot)=>snapshot
-    .docs
-    .map((document)=>BiletModel.fromFireStore(document.data()))
-    .toList());
+  Stream<List<BiletModel>> getBiletler() {
+    return firestore.collection('biletler').snapshots().map((snapshot) =>
+        snapshot.docs
+            .map((document) => BiletModel.fromFireStore(document.data()))
+            .toList());
 
     // Firestore Database icerisinden rezervasyonlari listelemek icin gereken fonksiyondur.
   }
 
-  Future<void> removeBilet(String id)
-  {
+  Future<void> removeBilet(String id) {
     return firestore.collection('biletler').doc(id).delete();
     // Firestore database icerisindeki rezervasyonu silmek icin gereken fonksiyondur.
   }
- Future<void> updateReservation(BiletModel biletModel) {
-  return firestore.collection('biletler').doc(biletModel.id).set(biletModel.toMap());
-  // Firestore database icerisindeki rezervasyonun verilerini guncellemek icin gereken fonksiyondur.
- }
+
+  Future<void> updateReservation(BiletModel biletModel) {
+    return firestore
+        .collection('biletler')
+        .doc(biletModel.id)
+        .set(biletModel.toMap());
+    // Firestore database icerisindeki rezervasyonun verilerini guncellemek icin gereken fonksiyondur.
+  }
 }
